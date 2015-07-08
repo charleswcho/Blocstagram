94eb7a6625e7ede919ab6f4942cb214a84359fab
Charles Wesley Cho <charleswcho@gmail.com>
2015-07-08 10:36:01 -0700

    Initial Blocstagram commit

---
+++ Blocstagram/Blocstagram/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Blocstagram
+//
+//  Created by Charles Wesley Cho on 7/8/15.
+//  Copyright (c) 2015 Charles Wesley Cho. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Blocstagram/Blocstagram/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Blocstagram
+//
+//  Created by Charles Wesley Cho on 7/8/15.
+//  Copyright (c) 2015 Charles Wesley Cho. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
