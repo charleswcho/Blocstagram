59e2b04dde033571b47b5f22912c1b601b9a59d7
Charles Wesley Cho <charleswcho@gmail.com>
2015-07-08 11:21:06 -0700

    Checkpoint 27 completed

--- Blocstagram/Blocstagram/ImagesTableViewController.h
+++ Blocstagram/Blocstagram/ImagesTableViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  ImagesTableViewController.h
 //  Blocstagram
 //
 //  Created by Charles Wesley Cho on 7/8/15.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface ImagesTableViewController : UITableViewController
 
 @end
-
