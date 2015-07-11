3f5e9f4f6074139131d9af35dc0ba4b8b6d308df
Charles Wesley Cho <charleswcho@gmail.com>
2015-07-11 12:55:05 -0700

    Assignment 31 completed

--- Blocstagram/Blocstagram/DataSource.m
+++ Blocstagram/Blocstagram/DataSource.m
@@ -69,9 +69,8 @@
 - (void) deleteMediaItem:(Media *)item {
     NSMutableArray *mutableArrayWithKVO = [self mutableArrayValueForKey:@"mediaItems"];
     [mutableArrayWithKVO removeObject:item];
+    [mutableArrayWithKVO insertObject:item atIndex:0];
 }
-
-
 
 - (void) addRandomData {
     NSMutableArray *randomMediaItems = [NSMutableArray array];
