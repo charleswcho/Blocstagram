d8ebaf62d79248b7db643b7fc46370ba192044b1
Charles Wesley Cho <charleswcho@gmail.com>
2015-07-17 20:57:05 -0700

    Checkpoint 42 completed

---
+++ Blocstagram/Blocstagram/CropBox.h
@@ -0,0 +1,13 @@
+//
+//  CropBox.h
+//  Blocstagram
+//
+//  Created by Charles Wesley Cho on 7/17/15.
+//  Copyright (c) 2015 Charles Wesley Cho. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CropBox : UIView
+
+@end
